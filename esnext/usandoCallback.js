a2c526e97570191cc7a4403a99ba932f8571fc5b
dainarakrewer <55606090+dainarakrewer@users.noreply.github.com>
2022-04-18 17:18:21 -0300

    18-04

--- esnext/usandoCallback.js
+++ esnext/usandoCallback.js
@@ -15,3 +15,19 @@ const getTurma = (letra, callback) => {
         })
     })
 }
+
+let nomes = []
+getTurma('A', alunos => {
+    //console.log(alunos[0].nome)
+    nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`))
+    //console.log(nomes)
+
+    getTurma('B', alunos => {
+        nomes = nomes.concat(alunos.map(a => `B: ${a.nome}`))
+        //console.log(nomes)
+        getTurma('C', alunos => {
+            nomes = nomes.concat(alunos.map(a => `C: ${a.nome}`))
+            console.log(nomes)
+        })
+    })
+})
