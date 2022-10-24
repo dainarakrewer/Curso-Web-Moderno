45efae2a95bff2935d050e33c1c3125cb2dffcc5
Dainara Krewer <55606090+dainarakrewer@users.noreply.github.com>
2022-10-24 16:12:33 -0300

    24-10

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -9,4 +9,10 @@ app.post('/usuarios', (req, resp) => {
     resp.send('Parabéns')
 })
 
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('Parabéns. Usuário Alterado!')
+})
+
 app.listen(3003)
