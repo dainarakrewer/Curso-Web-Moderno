9f21c6044a97025a6e538d4c6bc56f0bc3d85adb
Dainara Krewer <55606090+dainarakrewer@users.noreply.github.com>
2023-02-17 11:04:28 -0300

    17-02

---
+++ react/calculadora/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import './Button.css'
+
+export default props =>
+    <button className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+    `}
+    onClick={e => props.click(props.label)}>
+        {props.label}
+    </button>
