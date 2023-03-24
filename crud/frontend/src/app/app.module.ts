7f9401261b334e554d9db474b6abb32132ca7112
Dainara Krewer <55606090+dainarakrewer@users.noreply.github.com>
2023-03-24 11:16:05 -0300

    24-03

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -10,7 +10,12 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {FooterComponent} from './components/template/footer/footer.component';
 import {NavComponent} from './components/template/nav/nav.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
+import { MatCardModule} from "@angular/material/card"
 import {MatListModule} from "@angular/material/list";
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 
 @NgModule({
@@ -18,7 +23,11 @@ import {MatListModule} from "@angular/material/list";
         AppComponent,
         HeaderComponent,
         FooterComponent,
-        NavComponent
+        NavComponent,
+        HomeComponent,
+        ProductCrudComponent,
+        RedDirective,
+        ProductCreateComponent,
     ],
     imports: [
         BrowserModule,
@@ -26,7 +35,8 @@ import {MatListModule} from "@angular/material/list";
         BrowserAnimationsModule,
         MatToolbarModule,
         MatSidenavModule,
-        MatListModule
+        MatListModule,
+        MatCardModule
     ],
     providers: [],
     bootstrap: [AppComponent]
