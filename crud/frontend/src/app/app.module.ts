475f224f62b5f6ae84afe3beab27658e68ad3e36
Dainara Krewer <55606090+dainarakrewer@users.noreply.github.com>
2023-03-28 11:02:20 -0300

    28-03

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -27,7 +27,12 @@ import { ProductRead2Component } from './components/product/product-read2/produc
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort'
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component'
 
+registerLocaleData(localePt)
 
 @NgModule({
     declarations: [
@@ -41,6 +46,8 @@ import { MatSortModule } from '@angular/material/sort'
         ProductCreateComponent,
         ProductReadComponent,
         ProductRead2Component,
+        ProductUpdateComponent,
+        ProductDeleteComponent,
     ],
     imports: [
         BrowserModule,
@@ -60,7 +67,10 @@ import { MatSortModule } from '@angular/material/sort'
         MatPaginatorModule,
         MatSortModule
     ],
-    providers: [],
+    providers: [{
+        provide: LOCALE_ID,
+        useValue: 'pt-BR'
+    }],
     bootstrap: [AppComponent]
 })
 export class AppModule {
